42b35ae14b0439ec1406f4ed7a04b3f168384be1
Iain Maryanow <iainmaryanow@gmail.com>
2019-11-14 15:11:45 +0100

    Update for patients

--- Project/KDTree.py
+++ Project/KDTree.py
@@ -1,5 +1,3 @@
-import math
-
 class KDTree:
   def __init__(self, value=None, index=None, left_tree=None, right_tree=None):
     self._value = value
@@ -7,8 +5,11 @@ class KDTree:
     self._left_tree = left_tree
     self._right_tree = right_tree
 
+
   @staticmethod
-  def build(points, depth=0):
+  def build(patients, depth=0):
+    points = [patient.get_features() for patient in patients]
+
     if len(points) <= 0:
       return None
 
@@ -16,56 +17,54 @@ class KDTree:
     points.sort(key=lambda point: point[index_to_split])
 
     median_index = len(points) // 2
-    median_value = points[median_index]
-    left_tree = KDTree.build(points[:median_index], depth + 1)
-    right_tree = KDTree.build(points[median_index + 1:], depth + 1)
+    median_patient = patients[median_index]
+    left_tree = KDTree.build(patients[:median_index], depth + 1)
+    right_tree = KDTree.build(patients[median_index + 1:], depth + 1)
 
-    return KDTree(median_value, index_to_split, left_tree, right_tree)
+    return KDTree(median_patient, index_to_split, left_tree, right_tree)
 
-  @staticmethod
-  def distance(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-
-    dx = x1 - x2
-    dy = y1 - y2
-
-    return math.sqrt(dx * dx + dy * dy)
 
   def is_leaf(self):
     return self._left_tree is None and self._right_tree is None
 
+
   def get_value(self):
     return self._value
+
 
   def get_left_tree(self):
     return self._left_tree
 
+
   def get_right_tree(self):
     return self._right_tree
+
+
+  def predict_outcome(self, patient, max_distance, max_patients_in_grouping, *features):
+    nearest_patients = self.create_grouping(patient, max_distance, max_patients_in_grouping, *features)
+    outcomes = [patient.get_outcome() for patient in nearest_patients]
+    return max(set(outcomes), key=outcomes.count)
+
 
   def create_grouping(self, patient, max_distance, max_patients_in_grouping, *features):
     if max_distance <= 0 or max_patients_in_grouping < 1:
       return []
 
     nearest_patients = self._create_grouping(patient, max_distance, max_patients_in_grouping, features)
+    return sorted(nearest_patients, key=lambda p: p.compute_distance(patient, *features))[:max_patients_in_grouping]
 
-    # Only has the distance to the nearest patients, need to update to also return the patients
-    # so we can predict from the list of outcomes
-    return sorted(nearest_patients)[:max_patients_in_grouping]
 
-  # features isn't used yet, need to change when Patient object is implemented with a distance function
   def _create_grouping(self, patient, max_distance, max_patients_in_grouping, features, farthest_distance=None):
     nearest_patients = []
-    distance = KDTree.distance(self._value, patient)
+    distance = self._value.compute_distance(patient, *features)
 
     if distance <= max_distance:
-      nearest_patients.append(distance)
+      nearest_patients.append(self._value)
       if farthest_distance is None or distance > farthest_distance:
         farthest_distance = distance
 
-    comparision_value = self._value[self._index]
-    patient_value = patient[self._index]
+    comparision_value = self._value.get_features()[self._index]
+    patient_value = patient.get_features()[self._index]
     chosen_branch, unchosen_branch = self._get_branches(comparision_value, patient_value)
 
     if chosen_branch is not None:
@@ -90,6 +89,7 @@ class KDTree:
 
     return nearest_patients
 
+
   def _get_branches(self, comparision_value, patient_value):
     if patient_value < comparision_value:
       chosen_branch = self._left_tree
@@ -100,23 +100,25 @@ class KDTree:
 
     return chosen_branch, unchosen_branch
 
+
   def _should_check_unchosen_branch(self, farthest_distance, patient_distance, nearest_patients, max_patients_in_grouping):
     return (farthest_distance is not None and patient_distance <= farthest_distance) \
       or len(nearest_patients) < max_patients_in_grouping
 
-  # def print(self, indentation=0):
-  #   if self.is_leaf():
-  #     print(' ' * indentation, 'value for index', self._index, ':', self._value)
-  #   else:
-  #     print(' ' * indentation, 'median value for index', self._index, ':', self._value)
-  #     print(' ' * indentation, 'left')
-  #     if self._left_tree is None:
-  #       print(' ' * (indentation + 2), None)
-  #     else:
-  #       self._left_tree.print(indentation + 2)
 
-  #     print(' ' * indentation, 'right')
-  #     if self._right_tree is None:
-  #       print(' ' * (indentation + 2), None)
-  #     else:
-  #       self._right_tree.print(indentation + 2)
+  def print(self, indentation=0):
+    if self.is_leaf():
+      print(' ' * indentation, 'value for index', self._index, ':', self._value)
+    else:
+      print(' ' * indentation, 'median value for index', self._index, ':', self._value)
+      print(' ' * indentation, 'left')
+      if self._left_tree is None:
+        print(' ' * (indentation + 2), None)
+      else:
+        self._left_tree.print(indentation + 2)
+
+      print(' ' * indentation, 'right')
+      if self._right_tree is None:
+        print(' ' * (indentation + 2), None)
+      else:
+        self._right_tree.print(indentation + 2)
