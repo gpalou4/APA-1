73621a96f9f9ae6ddb5572d29c46043d29958301
gpalou4 <44896814+gpalou4@users.noreply.github.com>
2019-11-22 00:52:31 +0100

    Add files via upload

--- Project/patients.py
+++ Project/patients.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 class Patient:
 
@@ -12,13 +13,13 @@ class Patient:
     sex,
     length_of_hospital,
     bmi,
-    #number_of_medications,
     number_of_additives,
     number_of_procedures,
     duration_of_medications,
     percentage_of_abnormal_lab_events,
     comorbidity_score,
-    hospital_admission_id
+    hospital_admission_id,
+    medication_count
   ):
     self._patient_id= patient_id
     self._outcome = outcome
@@ -28,53 +29,53 @@ class Patient:
     self._sex = sex
     self._length_of_hospital = length_of_hospital
     self._bmi = bmi
-    #self._number_of_medications = number_of_medications
     self._number_of_additives = number_of_additives
     self._number_of_procedures = number_of_procedures
     self._duration_of_medications = duration_of_medications
     self._percentage_of_abnormal_lab_events = percentage_of_abnormal_lab_events
     self._comorbidity_score = comorbidity_score
-    self._hospital_admission_id = hospital_admission_id
+    self._hospital_admission_id = hospital_admission_id,
+    self._medication_count = medication_count
 
     self._features = (
       self._age,
       self._sex,
       self._length_of_hospital,
       self._bmi,
-      #self._number_of_medications,
-      self._comorbidity_score,
       self._number_of_additives,
       self._number_of_procedures,
       self._duration_of_medications,
-      self._percentage_of_abnormal_lab_events
+      self._percentage_of_abnormal_lab_events,
+      self._comorbidity_score,
+      self._medication_count
     )
 
   @staticmethod
   def parse_patients_from_file(filename):
     with open(filename,"r") as features:
+      features.readline()
+      features_csv = csv.reader(features, delimiter=',')
       patients = []
-      for line in features.readlines()[1:]:
-        parsed_line = line.strip().split(",")
+      for parsed_line in features_csv:
         patient_id = parsed_line[0]
-        outcome = str(parsed_line[1])
+        outcome = int(parsed_line[1])
         icustay_sequence = float(parsed_line[2])
         icustay_id = int(parsed_line[3])
         age = float(parsed_line[4])
         sex = int(parsed_line[5])
         length_of_hospital = float(parsed_line[6])
         bmi = float(parsed_line[7])
-        number_of_medications = parsed_line[8]
         number_of_additives = float(parsed_line[8])
         number_of_procedures = float(parsed_line[9])
         duration_of_medications = float(parsed_line[10])
         percentage_of_abnormal_lab_events = float(parsed_line[11])
         comorbidity_score = float(parsed_line[12])
-        hospital_admission_id = int(parsed_line[13])
+        hospital_admission_id = float(parsed_line[13])
+        medication_count = int(parsed_line[14])
         patient = Patient(patient_id, outcome, icustay_sequence, icustay_id, age, sex, length_of_hospital, bmi,
           number_of_additives, number_of_procedures, duration_of_medications, percentage_of_abnormal_lab_events, comorbidity_score, 
-          hospital_admission_id)
+          hospital_admission_id, medication_count)
         patients.append(patient)
-        features.close()
       return patients
 
   def get_outcome(self):
@@ -92,19 +93,54 @@ class Patient:
     return hasattr(self, '_' + feature)
 
   def compute_distance(self, patient, *features):
-    # if features:
-    list_of_features_1 = []
-    list_of_features_2 = []
+    features_1 = []
+    features_2 = []
     for feature in features:
       if not self.has_feature(feature):
-        raise Error('not found')
+        raise NameError('Feature not found')
+      features_1.append(self.get_feature())
+      features_2.append(patient.get_feature())
+    list_of_features_1 = features_1 or self.get_features()
+    list_of_features_2 = features_1 or patient.get_features()
+    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(list_of_features_1, list_of_features_2)]))
 
-      tuple_of_features_1 = list_of_features_1.append(self.get_feature())
-      tuple_of_features_2 = list_of_features_2.append(patient.get_feature())
 
-    tuple_of_features_1 = tuple_of_features_1 or self.get_features()
-    tuple_of_features_2 = tuple_of_features_2 or patient.get_features()
+  #### LAST VERSIONS ####
+    
+  # def compute_distance(self, patient, *features):
+  #   features_1 = []
+  #   features_2 = []
+  #   for feature in features:
+  #     if not self.has_feature(feature):
+  #       raise NameError('Feature not found')
+  #     list_of_features_1 = features_1.append(self.get_feature())
+  #     list_of_features_2 = features_2.append(patient.get_feature())
+  #   list_of_features_1 = list_of_features_1 or self.get_features()
+  #   list_of_features_2 = list_of_features_2 or patient.get_features()
+  #   return math.sqrt(sum([(a - b) ** 2 for a, b in zip(list_of_features_1, list_of_features_1)]))
 
-    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(tuple_of_features_1, tuple_of_features_2)]))
-    # else:
-    #   return math.sqrt(sum([(a - b) ** 2 for a, b in zip(self.get_features(), patient.get_features())]))
+  # def parse_patients_from_file(filename):
+  #   with open(filename,"r") as features:
+  #     patients = []
+  #     for line in features.readlines()[1:]:
+  #       parsed_line = line.strip().split(",")
+  #       patient_id = parsed_line[0]
+  #       outcome = int(parsed_line[1])
+  #       icustay_sequence = float(parsed_line[2])
+  #       icustay_id = int(parsed_line[3])
+  #       age = float(parsed_line[4])
+  #       sex = int(parsed_line[5])
+  #       length_of_hospital = float(parsed_line[6])
+  #       bmi = float(parsed_line[7])
+  #       number_of_additives = float(parsed_line[8])
+  #       number_of_procedures = float(parsed_line[9])
+  #       duration_of_medications = float(parsed_line[10])
+  #       percentage_of_abnormal_lab_events = float(parsed_line[11])
+  #       comorbidity_score = float(parsed_line[12])
+  #       hospital_admission_id = float(parsed_line[13])
+  #       medication_count = int(parsed_line[14])
+  #       patient = Patient(patient_id, outcome, icustay_sequence, icustay_id, age, sex, length_of_hospital, bmi,
+  #         number_of_additives, number_of_procedures, duration_of_medications, percentage_of_abnormal_lab_events, comorbidity_score, 
+  #         hospital_admission_id, medication_count)
+  #       patients.append(patient)
+  #     return patients
